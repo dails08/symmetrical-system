66a6e20e0d7e4eb91506f277abab8028ddc4fb3a
Chris Dailey <christopher.dailey@gmail.com>
2025-08-10 21:19:30 -0400

    Adds independent hue rotation timing per chamber.

--- client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/char-sheet-gunslinger.ts
+++ client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/char-sheet-gunslinger.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, Input } from '@angular/core';
-import { Gunslinger  } from '../../../../../../server/src/SlayerRoomState';
+import { Gunslinger, SlayerRoomState  } from '../../../../../../server/src/SlayerRoomState';
 import { ColyseusService } from '../../services/colyseusService';
 import { ERunes } from '../../../../../../common/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { animate, utils, stagger, createTimer } from 'animejs';
+import { getStateCallbacks } from 'colyseus.js';
 
 @Component({
   selector: 'app-char-sheet-gunslinger',
@@ -34,12 +35,57 @@ export class CharSheetGunslinger  implements AfterViewInit{
       private cjs: ColyseusService
     ){
       
+      this.cjs.room.then(room => {
+        const $ = getStateCallbacks<SlayerRoomState>(room);
+        // $(this.slayer).onChange(() => {
+        //   setTimeout(this.updateRuneAnimations, 100);
+        // })
+
+        $(this.slayer).listen("chamber1Rune", (value, previousValue) => {
+          setTimeout(() => this.updateRuneAnimation("One"),100);
+        })
+        $(this.slayer).listen("chamber2Rune", (value, previousValue) => {
+          setTimeout(() => this.updateRuneAnimation("Two"),100);
+        })
+        $(this.slayer).listen("chamber3Rune", (value, previousValue) => {
+          setTimeout(() => this.updateRuneAnimation("Three"),100);
+        })
+        $(this.slayer).listen("chamber4Rune", (value, previousValue) => {
+          setTimeout(() => this.updateRuneAnimation("Four"),100);
+        })
+        $(this.slayer).listen("chamber5Rune", (value, previousValue) => {
+          setTimeout(() => this.updateRuneAnimation("Five"),100);
+        })
+        $(this.slayer).listen("chamber6Rune", (value, previousValue) => {
+          setTimeout(() => this.updateRuneAnimation("Six"),100);
+        })
+      })
 
      
       
     }
 
-    updateRuneAnimation(){
+    updateRuneAnimation(chamber: string) {
+      console.log("Updating rune animation for " + ".chamber" + chamber + "Rune");
+      const changedChamber = utils.$(".chamber" + chamber + "Rune");
+      console.log(changedChamber.length);
+      const toInvert = Math.floor(Math.random()*100);
+      const toDelay = Math.floor(Math.random() * 1000);
+
+      animate(changedChamber, {
+        duration: 10000,
+        loop: -1,
+        delay: toDelay,
+        alternate: true, 
+        filter: [
+          'invert(' + toInvert + '%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(119%)',
+          'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(119%)'
+        ]
+      })
+    }
+
+    updateRuneAnimations(){
+      console.log("Updating rune animations");
       const loadeds = utils.$(".loadedRune");
       console.log(loadeds.length);
       loadeds.forEach(elem => {
@@ -68,8 +114,12 @@ export class CharSheetGunslinger  implements AfterViewInit{
 
     }
 
+    logChamberClick(chamber: number) {
+      console.log("Chamber clicked!: " + chamber);
+    }
+
     ngAfterViewInit(): void {
-      this.updateRuneAnimation();
+      this.updateRuneAnimations();
 
     }
 
