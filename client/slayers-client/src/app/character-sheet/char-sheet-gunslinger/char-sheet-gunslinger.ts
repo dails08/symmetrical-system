8e500bc3e2237e9807e0aed38860f730ae91f9af
Chris Dailey <christopher.dailey@gmail.com>
2025-08-11 10:22:18 -0400

    Functionality increases as a function of jank. This demands a rewrite.

--- client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/char-sheet-gunslinger.ts
+++ client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/char-sheet-gunslinger.ts
@@ -30,6 +30,12 @@ export class CharSheetGunslinger  implements AfterViewInit{
   }[] = [];
 
   stateReloading: boolean = false;
+  chamber1Staged = false;
+  chamber2Staged = false;
+  chamber3Staged = false;
+  chamber4Staged = false;
+  chamber5Staged = false;
+  chamber6Staged = false;
 
   constructor(
       private cjs: ColyseusService
@@ -66,22 +72,25 @@ export class CharSheetGunslinger  implements AfterViewInit{
     }
 
     updateRuneAnimation(chamber: string) {
-      console.log("Updating rune animation for " + ".chamber" + chamber + "Rune");
-      const changedChamber = utils.$(".chamber" + chamber + "Rune");
-      console.log(changedChamber.length);
-      const toInvert = Math.floor(Math.random() * 50) + 25;
-      const toDelay =  Math.floor(Math.random() * 1000);
-
-      animate(changedChamber, {
-        duration: 10000,
-        loop: -1,
-        delay: toDelay,
-        alternate: true,
-        filter: [
-          'invert(' + toInvert + '%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(119%)',
-          'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(119%)'
-        ]
-      })
+      setTimeout(() => {
+        console.log("Updating rune animation for " + ".chamber" + chamber + "Rune");
+        const changedChamber = utils.$(".chamber" + chamber + "Rune");
+        console.log(changedChamber.length);
+        const toInvert = Math.floor(Math.random() * 50) + 25;
+        const toDelay =  Math.floor(Math.random() * 1000);
+  
+        animate(changedChamber, {
+          duration: 10000,
+          loop: -1,
+          delay: toDelay,
+          alternate: true,
+          filter: [
+            'invert(' + toInvert + '%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(119%)',
+            'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(119%)'
+          ]
+        })
+      },100)
+      
     }
 
     updateRuneAnimations(){
@@ -120,11 +129,71 @@ export class CharSheetGunslinger  implements AfterViewInit{
 
     addToStaged(event: CdkDragDrop<{chamber: number, rune: ERunes}[]>){
       this.stagedRounds.push(event.item.data);
-      this.updateRuneAnimation(event.item.data.chamber);
+      switch (event.item.data.chamber){
+        case 1: {
+          this.chamber1Staged = true;
+          break;
+        }
+        case 2: {
+          this.chamber2Staged = true;
+          break;
+        }
+        case 3: {
+          this.chamber3Staged = true;
+          break;
+        }
+        case 4: {
+          this.chamber4Staged = true;
+          break;
+        }
+        case 5: {
+          this.chamber5Staged = true;
+          break;
+        }
+        case 6: {
+          this.chamber6Staged = true;
+          break;
+        }
+      }
+      this.updateRuneAnimation(["", "One", "Two", "Three", "Four", "Five", "Six"][event.item.data.chamber]);
+    }
+
+    chamberStaged(chamber: number) {
+      return this.stagedRounds.find((value) => {
+        value.chamber == chamber
+      })
     }
 
     unstage(index: number) {
+      const data = this.stagedRounds[index];
+      switch (data.chamber){
+        case 1: {
+          this.chamber1Staged = false;
+          break;
+        }
+        case 2: {
+          this.chamber2Staged = false;
+          break;
+        }
+        case 3: {
+          this.chamber3Staged = false;
+          break;
+        }
+        case 4: {
+          this.chamber4Staged = false;
+          break;
+        }
+        case 5: {
+          this.chamber5Staged = false;
+          break;
+        }
+        case 6: {
+          this.chamber6Staged = false;
+          break;
+        }
+      }
       this.stagedRounds.splice(index, 1);
+      this.updateRuneAnimation(["One", "Two", "Three", "Four", "Five", "Six"][data.chamber - 1]);
     }
 
     ngAfterViewInit(): void {
