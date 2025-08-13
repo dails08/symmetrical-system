751573660e6b9f6355f932fb7c2eb432b378fa99
Chris Dailey <christopher.dailey@gmail.com>
2025-08-13 11:15:35 -0400

    Functional cooperation with animejs, img loading, and loaded changes.

--- client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/chamber/chamber.ts
+++ client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/chamber/chamber.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild, viewChild } from '@angular/core';
 import { ERunes } from '../../../../../../../common/common';
 import { CommonModule } from '@angular/common';
+import { animate, utils } from 'animejs';
 
 @Component({
   selector: 'app-chamber',
@@ -8,7 +9,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './chamber.html',
   styleUrl: './chamber.scss'
 })
-export class Chamber {
+export class Chamber implements AfterViewInit {
 
   @Input({required: true})
   rune!: ERunes;
@@ -16,8 +17,53 @@ export class Chamber {
   @Input({required: true})
   loaded!: boolean;
 
-  unstaged = true;
+  @Input({required: true})
+  chamber!: number;
+
+  @ViewChild("runeImg")
+  runeImg!: HTMLElement;
+
+  selected = false;
 
   ERunes = ERunes;
+
+
+  updateRuneAnimation() {
+    const toDelay =  Math.floor(Math.random() * 3000);
+    if (this.runeImg){
+      console.log("Found rune image");
+      setTimeout(() => {
+        // console.log("Updating rune animation for chamber with " + this.rune);
+        // const changedChamber = utils.$(this.runeImg);
+        // console.log(changedChamber.length);
+        // const toInvert = Math.floor(Math.random() * 50) + 25;
+        const toDuration = Math.floor(Math.random() * 3000) + 5000;
+        // console.log(toDuration);
+        // const toContrast = Math.floor(Math.random() * 50) + 25;
+  
+        animate("#runeImg" + this.chamber, {
+          duration: toDuration,
+          loop: -1,
+          delay: toDelay,
+          alternate: true,
+          // width: "200px",
+          filter: [
+            'invert(42%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(119%)',
+            'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(119%)'
+          ]
+          // filter: [
+          //   'invert(' + toInvert + '%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(120%)',
+            // 'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(' + toContrast +  '%)'
+          // ]
+        })
+      },100);
+    } 
+    
+    
+  }
+
+  ngAfterViewInit(): void {
+    this.updateRuneAnimation();
+  }
 
 }
--- client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/char-sheet-gunslinger.ts
+++ client/slayers-client/src/app/character-sheet/char-sheet-gunslinger/char-sheet-gunslinger.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { Gunslinger, SlayerRoomState  } from '../../../../../../server/src/SlayerRoomState';
 import { ColyseusService } from '../../services/colyseusService';
 import { ERunes } from '../../../../../../common/common';
@@ -6,21 +6,36 @@ import { DragDropModule, CdkDragDrop } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { animate, utils, stagger, createTimer } from 'animejs';
 import { getStateCallbacks } from 'colyseus.js';
+import { Chamber } from './chamber/chamber';
 
 @Component({
   selector: 'app-char-sheet-gunslinger',
   imports: [
     CommonModule,
-    DragDropModule
-  ],
+    DragDropModule,
+    Chamber
+],
   templateUrl: './char-sheet-gunslinger.html',
   styleUrl: './char-sheet-gunslinger.scss'
 })
-export class CharSheetGunslinger  implements AfterViewInit{
+export class CharSheetGunslinger {
 
   @Input({required: true}) slayer!: Gunslinger
 
   ERunes = ERunes;
+
+  @ViewChild("chamber1")
+  firstChamber!: Chamber;
+  @ViewChild("chamber2")
+  secondChamber!: Chamber;
+  @ViewChild("chamber3")
+  thirdChamber!: Chamber;
+  @ViewChild("chamber4")
+  fourthChamber!: Chamber;
+  @ViewChild("chamber5")
+  fifthChamber!: Chamber;
+  @ViewChild("chamber6")
+  sixthChamber!: Chamber;
 
   rotationValue: number = 0;
 
@@ -29,13 +44,7 @@ export class CharSheetGunslinger  implements AfterViewInit{
     rune: ERunes
   }[] = [];
 
-  stateReloading: boolean = false;
-  chamber1Staged = false;
-  chamber2Staged = false;
-  chamber3Staged = false;
-  chamber4Staged = false;
-  chamber5Staged = false;
-  chamber6Staged = false;
+  stateReloading: boolean = false
 
   constructor(
       private cjs: ColyseusService
@@ -43,162 +52,48 @@ export class CharSheetGunslinger  implements AfterViewInit{
 
       this.cjs.room.then(room => {
         const $ = getStateCallbacks<SlayerRoomState>(room);
-        // $(this.slayer).onChange(() => {
-        //   setTimeout(this.updateRuneAnimations, 100);
+        // $(this.slaye r).onChange(() => {
+        //   // setTimeout(this.updateRuneAnimations, 100);
+        //   this.firstChamber.updateRuneAnimation();
+        //   this.secondChamber.updateRuneAnimation();
+        //   this.thirdChamber.updateRuneAnimation();
+        //   this.fourthChamber.updateRuneAnimation();
+        //   this.fifthChamber.updateRuneAnimation();
+        //   this.sixthChamber.updateRuneAnimation();
         // })
+        $(this.slayer).listen("chamber1Rune",(value, previousValue) => {
+          this.firstChamber.updateRuneAnimation();
+        })
+        $(this.slayer).listen("chamber2Rune",(value, previousValue) => {
+          this.secondChamber.updateRuneAnimation();
+        })
+        $(this.slayer).listen("chamber3Rune",(value, previousValue) => {
+          this.thirdChamber.updateRuneAnimation();
+        })
+        $(this.slayer).listen("chamber4Rune",(value, previousValue) => {
+          this.fourthChamber.updateRuneAnimation();
+        })
+        $(this.slayer).listen("chamber5Rune",(value, previousValue) => {
+          this.fifthChamber.updateRuneAnimation();
+        })
+        $(this.slayer).listen("chamber6Rune",(value, previousValue) => {
+          this.sixthChamber.updateRuneAnimation();
+        })
 
-        $(this.slayer).listen("chamber1Rune", (value, previousValue) => {
-          setTimeout(() => this.updateRuneAnimation("One"),100);
-        })
-        $(this.slayer).listen("chamber2Rune", (value, previousValue) => {
-          setTimeout(() => this.updateRuneAnimation("Two"),100);
-        })
-        $(this.slayer).listen("chamber3Rune", (value, previousValue) => {
-          setTimeout(() => this.updateRuneAnimation("Three"),100);
-        })
-        $(this.slayer).listen("chamber4Rune", (value, previousValue) => {
-          setTimeout(() => this.updateRuneAnimation("Four"),100);
-        })
-        $(this.slayer).listen("chamber5Rune", (value, previousValue) => {
-          setTimeout(() => this.updateRuneAnimation("Five"),100);
-        })
-        $(this.slayer).listen("chamber6Rune", (value, previousValue) => {
-          setTimeout(() => this.updateRuneAnimation("Six"),100);
-        })
-      })
+    })
+  }
 
-
-
-    }
-
-    updateRuneAnimation(chamber: string) {
-      setTimeout(() => {
-        console.log("Updating rune animation for " + ".chamber" + chamber + "Rune");
-        const changedChamber = utils.$(".chamber" + chamber + "Rune");
-        console.log(changedChamber.length);
-        const toInvert = Math.floor(Math.random() * 50) + 25;
-        const toDelay =  Math.floor(Math.random() * 1000);
   
-        animate(changedChamber, {
-          duration: 10000,
-          loop: -1,
-          delay: toDelay,
-          alternate: true,
-          filter: [
-            'invert(' + toInvert + '%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(119%)',
-            'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(119%)'
-          ]
-        })
-      },100)
-      
-    }
 
-    updateRuneAnimations(){
-      console.log("Updating rune animations");
-      const loadeds = utils.$(".loadedRune");
-      console.log(loadeds.length);
-      loadeds.forEach(elem => {
-        const toInvert = Math.floor(Math.random()*100);
-        const toDelay = Math.floor(Math.random() * 1000);
-        animate(elem, {
-          duration: 10000,
-          loop: -1,
-          delay: toDelay,
-          alternate: true,
-          filter: [
-            'invert(' + toInvert + '%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(119%)',
-            'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(119%)'
-          ]
-        })
-      })
-      // animate(loadeds, {
-      //   duration: 10000,
-      //   loop: -1,
-      //   delay: stagger(100),
-      //   filter: [
-      //     'invert(42%) sepia(93%) saturate(1352%) hue-rotate(0deg) brightness(119%) contrast(119%)',
-      //     'invert(42%) sepia(93%) saturate(1352%) hue-rotate(359deg) brightness(119%) contrast(119%)'
-      //   ]
-      // });
-
-    }
 
     logChamberClick(chamber: number) {
       console.log("Chamber clicked!: " + chamber);
     }
 
-    addToStaged(event: CdkDragDrop<{chamber: number, rune: ERunes}[]>){
-      this.stagedRounds.push(event.item.data);
-      switch (event.item.data.chamber){
-        case 1: {
-          this.chamber1Staged = true;
-          break;
-        }
-        case 2: {
-          this.chamber2Staged = true;
-          break;
-        }
-        case 3: {
-          this.chamber3Staged = true;
-          break;
-        }
-        case 4: {
-          this.chamber4Staged = true;
-          break;
-        }
-        case 5: {
-          this.chamber5Staged = true;
-          break;
-        }
-        case 6: {
-          this.chamber6Staged = true;
-          break;
-        }
-      }
-      this.updateRuneAnimation(["", "One", "Two", "Three", "Four", "Five", "Six"][event.item.data.chamber]);
+    toggleSelected(chamber: Chamber) {
+      chamber.selected = !chamber.selected
     }
 
-    chamberStaged(chamber: number) {
-      return this.stagedRounds.find((value) => {
-        value.chamber == chamber
-      })
-    }
-
-    unstage(index: number) {
-      const data = this.stagedRounds[index];
-      switch (data.chamber){
-        case 1: {
-          this.chamber1Staged = false;
-          break;
-        }
-        case 2: {
-          this.chamber2Staged = false;
-          break;
-        }
-        case 3: {
-          this.chamber3Staged = false;
-          break;
-        }
-        case 4: {
-          this.chamber4Staged = false;
-          break;
-        }
-        case 5: {
-          this.chamber5Staged = false;
-          break;
-        }
-        case 6: {
-          this.chamber6Staged = false;
-          break;
-        }
-      }
-      this.stagedRounds.splice(index, 1);
-      this.updateRuneAnimation(["One", "Two", "Three", "Four", "Five", "Six"][data.chamber - 1]);
-    }
-
-    ngAfterViewInit(): void {
-      this.updateRuneAnimations();
-
-    }
+    
 
 }
